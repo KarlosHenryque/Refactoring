406876cecf85e8036d06987013b00e26b861904b
KarlosHenryque <karloshenryquep@gmail.com>
2025-11-17 19:46:35 -0300

    Commit 10

--- Movie.java
+++ Movie.java
@@ -46,4 +46,11 @@ public class Movie {
 
         return result;
     }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        if (this.getPriceCode() == NEW_RELEASE && daysRented > 1) {
+            return 2;
+        }
+        return 1;
+    }
 }
