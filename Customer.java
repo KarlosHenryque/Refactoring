dc54feed8566607bd40c51f520601aa372ba53aa
KarlosHenryque <karloshenryquep@gmail.com>
2025-11-17 19:40:44 -0300

    Commit 6

--- Customer.java
+++ Customer.java
@@ -25,14 +25,9 @@ public class Customer {
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
 
-            frequentRenterPoints++;
+            frequentRenterPoints += each.getFrequentRenterPoints();
 
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-                    each.getDaysRented() > 1) {
-                frequentRenterPoints++;
-            }
-
-            result += "\t" + each.getMovie().getTitle() + "\t" + 
+            result += "\t" + each.getMovie().getTitle() + "\t" +
                       String.valueOf(each.getCharge()) + "\n";
 
             totalAmount += each.getCharge();
